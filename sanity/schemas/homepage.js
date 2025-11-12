6abe69dc885c68c2f2256d86a14ef1070fbc2e31
Mejor-Vida <julie@mejorvidainsurance.com>
2025-11-11 21:53:31 -0600

    Create homepage.js

---
+++ sanity/schemas/homepage.js
@@ -0,0 +1,27 @@
+export default {
+  name: 'homepage',
+  title: 'Homepage',
+  type: 'document',
+  fields: [
+    {
+      name: 'title_es',
+      title: 'Título (Español)',
+      type: 'string',
+    },
+    {
+      name: 'title_en',
+      title: 'Title (English)',
+      type: 'string',
+    },
+    {
+      name: 'subtitle_es',
+      title: 'Subtítulo (Español)',
+      type: 'text',
+    },
+    {
+      name: 'subtitle_en',
+      title: 'Subtitle (English)',
+      type: 'text',
+    },
+  ],
+}
